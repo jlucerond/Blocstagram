25c89ee1c199b51de35c35cff627fabd862b6766
Joe Lucero <jlucerond@gmail.com>
2015-05-14 21:15:40 -0400

    Checkpoint 29 Completed
    Merging this back to master.

--- Blocstagram/MediaTableViewCell.m
+++ Blocstagram/MediaTableViewCell.m
@@ -39,7 +39,7 @@ static NSParagraphStyle *paragraphStyle;
     usernameLabelGray = [UIColor colorWithRed:0.933 green:0.933 blue:0.933 alpha:1]; /*#eeeeee*/
     commentLabelGray = [UIColor colorWithRed:0.898 green:0.898 blue:0.898 alpha:1]; /*#e5e5e5*/
     linkColor = [UIColor colorWithRed:0.345 green:0.314 blue:0.427 alpha:1]; /*#58506d*/
-    topCommentColor = [UIColor orangeColor]; /*#FF8C00*/
+    topCommentColor = [UIColor orangeColor]; /*orange*/
     
     NSMutableParagraphStyle *mutableParagraphStyle = [[NSMutableParagraphStyle alloc] init];
     mutableParagraphStyle.headIndent = 20.0;

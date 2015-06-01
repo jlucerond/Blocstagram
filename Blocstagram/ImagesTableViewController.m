fb1fe0645da5ae7f99e49e33b41190daadad273f
Joe Lucero <jlucerond@gmail.com>
2015-06-01 09:55:06 -0400

    Checkpoint 38 Completed
    Added Liking & Unliking to Blocstagram

--- Blocstagram/ImagesTableViewController.m
+++ Blocstagram/ImagesTableViewController.m
@@ -186,6 +186,18 @@
     }
 }
 
+- (void) cellDidPressLikeButton:(MediaTableViewCell *)cell {
+    Media *item = cell.mediaItem;
+    
+    [[DataSource sharedInstance] toggleLikeOnMediaItem:item withCompletionHandler:^{
+        if (cell.mediaItem == item) {
+            cell.mediaItem = item;
+        }
+    }];
+    
+    cell.mediaItem = item;
+}
+
 
 
 /*
--- Blocstagram/Media.m
+++ Blocstagram/Media.m
@@ -46,6 +46,10 @@
             }
         
         self.comments = commentsArray;
+        
+        BOOL userHasLiked = [mediaDictionary[@"user_has_liked"] boolValue];
+        
+        self.likeState = userHasLiked ? LikeStateLiked : LikeStateNotLiked;
         }
     
     return self;
@@ -72,7 +76,7 @@
         }
         self.caption = [aDecoder decodeObjectForKey:NSStringFromSelector(@selector(caption))];
         self.comments = [aDecoder decodeObjectForKey:NSStringFromSelector(@selector(comments))];
-
+        self.likeState = [aDecoder decodeIntegerForKey:NSStringFromSelector(@selector(likeState))];
     }
     
     return self;
@@ -84,6 +88,7 @@
     [aCoder encodeObject:self.mediaURL forKey:NSStringFromSelector(@selector(mediaURL))];
     [aCoder encodeObject:self.caption forKey:NSStringFromSelector(@selector(caption))];
     [aCoder encodeObject:self.comments forKey:NSStringFromSelector(@selector(comments))];
+    [aCoder encodeInteger:self.likeState forKey:NSStringFromSelector(@selector(likeState))];
 }
 
 
